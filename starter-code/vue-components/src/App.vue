bf2d7058c0c706050a2856b63131ae068761633f
Raimonds Kazuss <rkazush@gmail.com>
2021-08-19 11:27:48 +0300

    updated logo and main page

--- starter-code/vue-components/src/App.vue
+++ starter-code/vue-components/src/App.vue
@@ -1,6 +1,6 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Vue logo" src="./assets/logo.jpg">
+  <HelloWorld msg="Welcome to Your Duck App"/>
 </template>
 
 <script>
---
+++ starter-code/vue-components/src/components/Header.vue
@@ -0,0 +1,18 @@
+/*
+hedaer contents:
+logo
+nav
+plus button
+user button
+*/
+
+<template>
+    <header style="color: tomato;">this is a header</header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  props: {}
+}
+</script>
